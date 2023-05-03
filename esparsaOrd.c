6be561fd9b2ae0e2d0006c2a16f6b8118b3d3738
Rafael Palheta Tokairin <108299374+rafatokairin@users.noreply.github.com>
2023-05-03 18:54:02 -0300

    Update esparsaOrd.c

--- esparsaOrd.c
+++ esparsaOrd.c
@@ -48,6 +48,8 @@ int main()
     info(&C);
     printf("\nSoma abaixo diagonal principal multiplicacao: %d\n", somaAbaixoDiag(&C));
     reiniciar(&A); reiniciar(&B); reiniciar(&C);
+    // libera primeiro arranjo
+    free(A.arranjo); free(B.arranjo); free(C.arranjo);
     return 0;
 }
 
